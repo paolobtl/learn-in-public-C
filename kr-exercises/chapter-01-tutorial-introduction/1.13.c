c90d9656e85a9d01cc1286c15761fc46ec2f5a94
paolobtl <bietolinipaolo@gmail.com>
2026-01-01 20:13:36 +0100

    refactor 1-13 to print a vertical istogram

--- kr-exercises/chapter-01-tutorial-introduction/1.13.c
+++ kr-exercises/chapter-01-tutorial-introduction/1.13.c
@@ -6,33 +6,46 @@
 
 #include <stdio.h>
 
-#define MAXLEN 10
+#define MAXLEN 20 
 
 int main(void) {
-  int c, w = 0;
-  int ndigit[MAXLEN] = {0};
+    int c, w = 0;
+    int ndigit[MAXLEN] = {0};
 
-  while ((c = getchar()) != EOF) {
-    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
-      w++;
-    } else {
-      if (w > 0 && w <= MAXLEN) {
-        ndigit[w - 1]++;
-      }
-      w = 0;
+    while ((c = getchar()) != EOF) {
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+            w++;
+        } else {
+            if (w > 0 && w <= MAXLEN)
+                ndigit[w - 1]++;
+            w = 0;
+        }
     }
-  }
+    if (w > 0 && w <= MAXLEN)
+        ndigit[w - 1]++;
 
-  if (w > 0 && w <= MAXLEN)
-    ndigit[w - 1]++;
+    int max = 0;
+    for (int i = 0; i < MAXLEN; i++)
+        if (ndigit[i] > max)
+            max = ndigit[i];
 
-  for (int i = 0; i < MAXLEN; i++) {
-    printf("%2d | ", i + 1);
-    for (int j = 0; j < ndigit[i]; j++)
-      printf("*");
+    for (int row = max; row > 0; row--) {
+        for (int col = 0; col < MAXLEN; col++) {
+            if (ndigit[col] >= row)
+                printf("  * ");
+            else
+                printf("    ");
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < MAXLEN; i++)
+        printf("----");
     printf("\n");
-  }
 
+    for (int i = 0; i < MAXLEN; i++)
+        printf("%3d ", i + 1);
+    printf("\n");
 
-  return 0;
+    return 0;
 }
